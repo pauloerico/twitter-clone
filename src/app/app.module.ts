65b947f8e47c8713d4ca5f12d1edad126a7ecf9e
Paulo <deoliveira.pe@gmail.com>
2023-04-17 20:12:22 -0300

    refactor: Rename component post to new-post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { PostComponent } from './components/post/post.component';
 import { TweetComponent } from './components/tweet/tweet.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewPostComponent } from './components/new-post/new-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PostComponent,
-    TweetComponent
+    TweetComponent,
+    HomeComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule
