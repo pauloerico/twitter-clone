bb483f41d90dd7b8c4bd8c793697462bf0be1111
Paulo <deoliveira.pe@gmail.com>
2023-04-17 19:31:14 -0300

    feat: Components header, post and tweet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostComponent } from './components/post/post.component';
+import { TweetComponent } from './components/tweet/tweet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostComponent,
+    TweetComponent
   ],
   imports: [
     BrowserModule
